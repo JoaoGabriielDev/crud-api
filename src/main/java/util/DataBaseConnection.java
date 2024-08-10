633776631fe1b17ba7e32c74f348eca4620b904d
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-08-10 14:51:42 -0300

    Conexão com o banco de dados

--- src/main/java/util/DataBaseConnection.java
+++ src/main/java/util/DataBaseConnection.java
@@ -1,4 +1,23 @@
 package main.java.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/crud";
+    private static final String USER = "joao";
+    private static final String PASSWORD ="";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
